686ce1ce1dbd00e4094c953a9cdc3c99fd4f8cdb
Pallavi Bhaskar <pallavi3885@gmail.com>
2014-03-25 16:30:23 +0000

    Commiting Seesion5 rails

---
+++ spec/user_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+	describe User do 
+
+		user = User.new
+
+		it "User name should be Bob" do
+			expect(user.name).to eq 'Bob'
+		end
+
+		it "User role should include admin" do
+			expect(user.roles).to include('admin')
+		end
+
+		it "User's postcode should be WC1 8CD" do
+			
+			user.postcode = 'WC1 8CD'
+			expect(user.postcode).to eq 'WC1 8CD'
+		end
+
+		it "User role should be admin" do
+			expect(user).to be_admin
+		end
+
+		it "User - empty param raises error" do
+			expect{user.add_role()}.to raise_error
+		end
+
+
+			it {should respond_to(:age, :name, :roles, :postcode)}
+
+ end
